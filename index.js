2def5ce810bc61ac9895b04b64af620522b26bca
Ruhul Amin <ruhuliuice@gmail.com>
2021-11-09 21:55:28 +0600

    change time

--- index.js
+++ index.js
@@ -47,7 +47,7 @@ async function run() {
     //query code
     app.get("/appointments", async (req, res) => {
       const email = req.query.email;
-      const date = new Date(req.query.date).toLocaleDateString();
+      const date = req.query.date;
       const query = { email: email, date: date };
       const cursor = appointmentCollection.find(query);
       const result = await cursor.toArray();
