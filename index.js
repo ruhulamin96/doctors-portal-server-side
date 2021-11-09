afa6f449948194aaa96832d23bf3c120de1988a0
Ruhul Amin <ruhuliuice@gmail.com>
2021-11-09 19:43:50 +0600

    test

--- index.js
+++ index.js
@@ -116,7 +116,7 @@ async function run() {
 run().catch(console.dir);
 
 app.get("/", (req, res) => {
-  res.send("Hello World!");
+  res.send("Hello doctors portal!");
 });
 
 app.listen(port, () => {
